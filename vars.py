e46faafafdac5012497941e49d91ef213ef3d592
Sujoy ॐ <iamsujoyp@gmail.com>
2025-09-25 18:55:21 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -4,10 +4,11 @@ import os
 
 API_ID    = os.environ.get("API_ID", "11039752")
 API_HASH  = os.environ.get("API_HASH", "c4141ad06d1f37b105841412c1aeb383")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
-ADMIN     = os.environ.get("ADMIN", "2032422349")  
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7963256849:AAFiLQbo-1_EZ7Ivt7Mh6i3z9sgojwyZWLI") 
+ADMIN     = os.environ.get("ADMIN", "6165653954")  
 ADMINS = [2032422349]  # Replace with your real Telegram ID
 # Replace with your Telegram user ID
 
 #WEBHOOK = True  # Don't change this
 #PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
+
